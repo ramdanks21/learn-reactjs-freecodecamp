954f36fa85c315c8d235993894077c0b23d63be4
ramdan kusumah <ramdankusumah2@gmail.com>
2024-02-09 11:33:23 +0700

    Render Komponen Kelas ke DOM

--- eps-12.jsx
+++ eps-12.jsx
@@ -6,46 +6,14 @@ class TypesOfFood extends React.Component {
     return (
       <div>
         <h1>Types of Food:</h1>
-        {/* Change code below this line */}
+        {/* change code below this line */}
         <Fruits />
         <Vegetables />
-        {/* Change code above this line */}
+        {/* change code above this line */}
       </div>
     );
   }
 }
 
-// Change code below this line
-class Fruits extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <h1>Fruits:</h1>
-        {/* Change code below this line */}
-
-        {/* Change code above this line */}
-      </div>
-    );
-  }
-}
-
-// Change code below this line
-class Vegetables extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <h1>Vegetable:</h1>
-        {/* Change code below this line */}
-
-        {/* Change code above this line */}
-      </div>
-    );
-  }
-}
-ReactDOM.render(<TypesOfFood />, document.getElementById("challaenge-node"));
+// change code below this line
+ReactDOM.render(<TypesOfFood />, document.getElementById("challenge-node"));

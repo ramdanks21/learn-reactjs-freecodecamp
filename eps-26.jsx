9b1fbf7b1ec99f825a8c20a10b3183c6ad2d9614
ramdan kusumah <ramdankusumah2@gmail.com>
2024-04-02 14:22:51 +0700

    create inline css using object

---
+++ eps-26.jsx
@@ -0,0 +1,13 @@
+// Change code above this line
+const styles = {
+  color: "purple",
+  fontSize: 40,
+  border: "2px solid purple",
+};
+class Colorful extends React.Component {
+  render() {
+    // Change code below this line
+    return <div style={styles}>Style Me!</div>;
+    // Change code above this line
+  }
+}
